f0e8063cbc0dd73868f4149de3e4473c1c2fdff0
qbettin <qbettin@gmail.com>
2024-10-19 16:27:49 -0400

    why

--- commands.py
+++ commands.py
@@ -7,6 +7,5 @@ from models import db, User, Todo  # Adjust the import based on your project str
 @click.command(name='create_tables')
 @with_appcontext
 def create_tables():
-    """Create database tables."""
     db.create_all()
     click.echo("Tables created!")
